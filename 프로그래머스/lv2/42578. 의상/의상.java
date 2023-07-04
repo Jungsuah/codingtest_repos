0d25ee6543be1b91d5c43419e8594811d7ccac48
정수아 <110010716+Jungsuah@users.noreply.github.com>
2023-07-04 13:18:17 +0900

    [level 2] Title: 의상, Time: 0.20 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 의상/의상.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 0;
+        HashMap <String,Integer> map = new HashMap<>();
+        for(int i = 0; i < clothes.length; i++){
+               map.put(clothes[i][1], map.getOrDefault(clothes[i][1],1)+1);
+            
+        }
+        
+        System.out.println(map);
+        int num  = 1;
+        for(int a : map.values()){
+           num = num * a; 
+        }
+        answer = num -1;
+        return answer;
+    }
+}
